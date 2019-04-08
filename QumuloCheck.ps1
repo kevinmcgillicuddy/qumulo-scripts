04f087a3c98891048f7d22007b0647e65c34563a
Kevin McGillicuddy <kevin.mcgillicuddy@sight-sound.com>
2019-04-08 15:11:54 -0400

    initial commit

---
+++ QumuloCheck.ps1
@@ -0,0 +1,54 @@
+﻿
+
+
+###########################
+# Enable HTTPS
+###########################
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+###########################
+# Get Token
+###########################
+
+
+$username = "admin"
+$password = Get-Content "C:\Temp\QPassword.txt" | ConvertTo-SecureString
+
+$bodydata = @{
+    username = $username
+	password = $password
+}
+
+$body = $bodydata | convertto-json
+
+$token = Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/session/login" -Method Post -Body $body -ContentType 'application/json'
+$token = $token.bearer_token | Out-String
+
+$header = @{
+    Authorization = "Bearer $token"
+}
+
+
+###########################
+###########################
+$alert=0
+$replicationdetail=Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/replication/source-relationships/status/" -method GET -Headers $header | convertto-json
+$hashtable = @{}
+(ConvertFrom-Json $replicationdetail).psobject.properties  |  Foreach { $hashtable[$_.Name] = $_.Value }
+foreach ($id in $hashtable.value.error_from_last_job){
+if ($id) {$alert=2} else {"empty"}
+}
+
+
+write-host "<prtg>"
+write-host "<result>"
+write-host "<channel>Replication Errors</channel>"
+write-host "<value>$alert</value>"
+write-host "<LimitMode>1</LimitMode>"
+write-host "<limitmaxerror>1</limitmaxerror>"
+write-host "</result>"
+write-host "</prtg>"
+
+
---
+++ Qumulo_Replication.ps1
@@ -0,0 +1,57 @@
+﻿
+
+
+###########################
+# Enable HTTPS
+###########################
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+###########################
+# Get Token
+###########################
+
+$username = "admin"
+$password = Get-Content "C:\Temp\QPassword.txt" | ConvertTo-SecureString
+
+$bodydata = @{
+    username = $username
+	password = $password
+}
+
+$body = $bodydata | convertto-json
+$token = Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/session/login" -Method Post -Body $body -ContentType 'application/json'
+$token = $token.bearer_token | Out-String
+
+$header = @{
+    Authorization = "Bearer $token"
+}
+
+
+###########################
+###########################
+
+
+$runningLong=$false
+$replicationdetail=Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/replication/source-relationships/status/" -method GET -Headers $header | convertto-json
+$hashtable = @{}
+(ConvertFrom-Json $replicationdetail).psobject.properties  |  Foreach { $hashtable[$_.Name] = $_.Value }
+
+foreach ($rep in $hashtable.Value.job_start_tim){
+$startdate = [datetime]::parse($hashtable.Value.job_start_time)
+$currentdate = date
+$totalruntime = $currentdate - $startdate
+#if ($totalruntime.TotalMinutes > 240){$runningLong=$true}
+}
+
+
+
+write-host "<prtg>"
+write-host "<result>"
+write-host "<channel>Replication Time Minutes</channel>"
+write-host "<value>"$totalruntime.minutes"</value>"
+write-host "<LimitMode>1</LimitMode>"
+write-host "<limitmaxerror>200</limitmaxerror>"
+write-host "</result>"
+write-host "</prtg>"
