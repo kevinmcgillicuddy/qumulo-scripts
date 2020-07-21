c2deb4506a6d4d5c5448664d70e573f237cb15df
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2020-07-20 22:39:59 -0400

    made url variable

--- QumuloCheck.ps1
+++ QumuloCheck.ps1
@@ -4,7 +4,7 @@
 
 $username = "admin"
 $password = Get-Content "C:\Temp\QPassword.txt" | ConvertTo-SecureString
-
+$url_base = 'ENTER_HERE FQDN'
 $bodydata = @{
     username = $username
 	password = $password
@@ -12,7 +12,7 @@ $bodydata = @{
 
 $body = $bodydata | convertto-json
 
-$token = Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/session/login" -Method Post -Body $body -ContentType 'application/json'
+$token = Invoke-RestMethod -Uri "https://$url_base:8000/v1/session/login" -Method Post -Body $body -ContentType 'application/json'
 $token = $token.bearer_token | Out-String
 
 $header = @{
@@ -21,7 +21,7 @@ $header = @{
 
 
 $alert=0
-$replicationdetail=Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/replication/source-relationships/status/" -method GET -Headers $header | convertto-json
+$replicationdetail=Invoke-RestMethod -Uri "https://$url_base:8000/v1/replication/source-relationships/status/" -method GET -Headers $header | convertto-json
 $hashtable = @{}
 (ConvertFrom-Json $replicationdetail).psobject.properties  |  Foreach { $hashtable[$_.Name] = $_.Value }
 foreach ($id in $hashtable.value.error_from_last_job){
--- Qumulo_Replication.ps1
+++ Qumulo_Replication.ps1
@@ -4,14 +4,14 @@
 
 $username = "admin"
 $password = Get-Content "C:\Temp\QPassword.txt" | ConvertTo-SecureString
-
+$url_base = 'ENTER_HERE FQDN'
 $bodydata = @{
     username = $username
 	password = $password
 }
 
 $body = $bodydata | convertto-json
-$token = Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/session/login" -Method Post -Body $body -ContentType 'application/json'
+$token = Invoke-RestMethod -Uri "https://$url_base:8000/v1/session/login" -Method Post -Body $body -ContentType 'application/json'
 $token = $token.bearer_token | Out-String
 
 $header = @{
@@ -20,7 +20,7 @@ $header = @{
 
 
 $runningLong=$false
-$replicationdetail=Invoke-RestMethod -Uri "https://st-qcluster01.bibleonstage.net:8000/v1/replication/source-relationships/status/" -method GET -Headers $header | convertto-json
+$replicationdetail=Invoke-RestMethod -Uri "https://$url_base:8000/v1/replication/source-relationships/status/" -method GET -Headers $header | convertto-json
 $hashtable = @{}
 (ConvertFrom-Json $replicationdetail).psobject.properties  |  Foreach { $hashtable[$_.Name] = $_.Value }
 
